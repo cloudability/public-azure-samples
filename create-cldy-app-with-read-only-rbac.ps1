24ca8f6f5a009378e04dedbabdd06df049304d5c
aloksingh <aloksingh@gmail.com>
2017-08-15 12:46:33 -0700

    Use the newest version of Azure powershell
    * Update Azure powershell version
    * Handle cases where the app/service principal already exists

--- create-cldy-app-with-read-only-rbac.ps1
+++ create-cldy-app-with-read-only-rbac.ps1
@@ -1,4 +1,5 @@
 <#
+Prerequisite: Azure RM v 4.2.0
 A Powershell script that creates a Application principal and assigns it a RBAC role for access to read-only 
 resources in Azure.
 #>
@@ -17,15 +18,22 @@ Function GET-Temppassword() {
 }
 
 
-$appPassword = GET-Temppassword(32)
-$sp = New-AzureRmADServicePrincipal -DisplayName "CloudabilityIntegrationReadOnlyApplication" -Password $appPassword
-#Sleep here for a few seconds to allow the service principal to propogate
-Echo "Waiting 30 seconds for service principal to propogate"
-Sleep 30
 
-$app = Get-AzureRmADApplication -ApplicationId $sp.ApplicationId.Guid
+$app = Get-AzureRmADServicePrincipal -SearchString 'CloudabilityIntegrationReadOnlyApplication'
+$appPassword = $null
+if($app -eq $null){	
+	$appPassword = GET-Temppassword(32)
+	$sp = New-AzureRmADServicePrincipal -DisplayName "CloudabilityIntegrationReadOnlyApplication" -Password $appPassword
+	#Sleep here for a few seconds to allow the service principal to propogate
+	Echo "Waiting 30 seconds for service principal to propogate"
+	Sleep 30
+	$app = Get-AzureRmADApplication -ApplicationId $sp.ApplicationId.Guid
+}else{
+	$appPassword = "NA"
+	Echo "$app exists"
+}
+
 $appId = $app.ApplicationId
-$appObjectId = $app.ObjectId.Guid
 
 <#
 For more information on Reader role, see: https://docs.microsoft.com/en-us/azure/active-directory/role-based-access-built-in-roles#reader
@@ -33,9 +41,15 @@ For more information on Reader role, see: https://docs.microsoft.com/en-us/azure
 $subs = Get-AzureRmSubscription
 $roleName = "Reader"
 foreach ($sub in $subs) {
-	$subId = $sub.SubscriptionId
-	Set-AzureRmContext -SubscriptionName $sub.SubscriptionName
-	New-AzureRMRoleAssignment -RoleDefinitionName $roleName  -ServicePrincipalName $sp.ApplicationId -Scope "/subscriptions/$subId"
+	$subId = $sub.Id
+	Set-AzureRmContext -SubscriptionName $sub.Name
+	$roles = Get-AzureRMRoleAssignment -RoleDefinitionName $roleName  -ServicePrincipalName $appId -Scope "/subscriptions/$subId"
+	if($roles -eq $null){
+		Echo "Creating a new role assignment $roleName for subscription: $subId"
+		New-AzureRMRoleAssignment -RoleDefinitionName $roleName  -ServicePrincipalName $appId -Scope "/subscriptions/$subId"
+	}else{
+		Echo "$roles exists for subscription $subId"
+	}
 }
 
 $tenantId = $subs[0].TenantId
